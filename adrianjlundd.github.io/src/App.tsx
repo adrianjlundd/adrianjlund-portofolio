c7b887efafff5e1f1d61cbdbac15fa351d75f8d6
Adrian Jacobsen Lund <adriajl@stud.ntnu.no>
2025-12-23 21:52:39 +0100

    Add base layout component

--- adrianjlundd.github.io/src/App.tsx
+++ adrianjlundd.github.io/src/App.tsx
@@ -1,8 +1,10 @@
+import Layout from "./components/Layout";
+
 export default function App() {
   return (
-    <main>
-      <h1 className="text-2xl font-bold">Portfolio - Adrian Jacobsen Lund</h1>
+    <Layout>
+      <h1 className="text-2xl font-bold text-red-500">Portfolio - Adrian Jacobsen Lund</h1>
       <h2 className="text-xl mt-4">Computer Science student @ NTNU</h2>
-    </main>
+    </Layout>
   )
 }
